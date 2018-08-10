697e78d03ad0d30c1b2e8f6c44cc504a54d534e0
Julian Hüpppauff <julian@hueppauff.info>
2018-08-10 11:41:03 +0200

    added unit test, added base logic

---
+++ Hangman/Hangman.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Hangman
+{
+    public class Hangman
+    {
+        private const char hyphen = '-';
+
+        private readonly string word;
+        private readonly char[] result;
+
+        public Hangman(string word)
+        {
+            if (word.Contains(hyphen))
+            {
+                throw new ArgumentException($"Word must not contain hyphen '{hyphen}'");
+            }
+
+            this.word = word;
+            this.result = new string(hyphen, this.word.Length).ToCharArray();
+        }
+
+        public bool Guess(char ch)
+        {
+            bool guessedCorrect = false;
+
+            for (int i = 0; i < this.word.Length; i++)
+            {
+                if (char.ToLower(this.word[i]) == char.ToLower(ch))
+                {
+                    this.result[i] = word[i];
+                    guessedCorrect = true;
+                }
+            }
+
+            return guessedCorrect;
+        }
+
+        public string Result => new string(this.result);
+
+        public bool Guessed => !this.result.Contains(hyphen);
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IntegrationTest()
+        {
+            var sut = new Hangman.Hangman("Developer");
+
+          
+        }
+    }
+}
