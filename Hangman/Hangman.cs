cd4400670604cf7358382823708921b5bc65085e
Julian Hüpppauff <julian@hueppauff.info>
2018-08-11 09:15:51 +0200

    added summaries

--- Hangman/Hangman.cs
+++ Hangman/Hangman.cs
@@ -3,13 +3,30 @@ using System.Linq;
 
 namespace Hangman
 {
+    /// <summary>
+    /// Hangman Base Class
+    /// </summary>
     public class Hangman
     {
+        /// <summary>
+        /// Delimiter for the Guessed Word
+        /// </summary>
         private const char hyphen = '-';
 
+        /// <summary>
+        /// Word which should be guessed
+        /// </summary>
         private readonly string word;
+
+        /// <summary>
+        /// Result which was already guessed
+        /// </summary>
         private readonly char[] result;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hangman"/> class.
+        /// </summary>
+        /// <param name="word"></param>
         public Hangman(string word)
         {
             if (word.Contains(hyphen))
@@ -21,6 +38,11 @@ namespace Hangman
             this.result = new string(hyphen, this.word.Length).ToCharArray();
         }
 
+        /// <summary>
+        /// Guesses a new Character
+        /// </summary>
+        /// <param name="ch">Character which will be guessed</param>
+        /// <returns>Result after the Character was validated against the unknown word</returns>
         public string Guess(char ch)
         {
             for (int i = 0; i < this.word.Length; i++)
@@ -34,8 +56,14 @@ namespace Hangman
             return new string(this.result);
         }
 
+        /// <summary>
+        /// Gets the Result
+        /// </summary>
         public string Result => new string(this.result);
 
+        /// <summary>
+        /// Indicates if the word has been guessed
+        /// </summary>
         public bool Guessed => !this.result.Contains(hyphen);
     }
 }
